86c3481f7dc6e3345f08e0913d904829d2582985
hyperld <nikolai1401@gmail.com>
2025-11-12 14:36:46 +0100

    Add additional exam entries to the exam list

--- lib/screens/exam_list_screen.dart
+++ lib/screens/exam_list_screen.dart
@@ -27,6 +27,36 @@ class ExamListScreen extends StatelessWidget {
       dateTime: DateTime(2022, 2, 18, 10, 30),
       rooms: ["Лаб 5"],
     ),
+    Exam(
+      subject: "Дигитизација",
+      dateTime: DateTime(2023, 2, 10, 9, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
+    Exam(
+      subject: "Вовед во наука за податоци",
+      dateTime: DateTime(2024, 6, 20, 12, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
+    Exam(
+      subject: "Објектно Ориентирано Програмирање",
+      dateTime: DateTime(2023, 2, 11, 15, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
+    Exam(
+      subject: "Структурно Програмирање",
+      dateTime: DateTime(2026, 2, 16, 9, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
+    Exam(
+      subject: "Интернет Технологии",
+      dateTime: DateTime(2026, 2, 12, 9, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
+    Exam(
+      subject: "Веб Програмирање",
+      dateTime: DateTime(2022, 3, 3, 9, 0),
+      rooms: ["Лаб 1", "Лаб 2"],
+    ),
   ];
 
   @override
